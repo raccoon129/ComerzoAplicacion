016cbe217a86372b0817e45d9a587c5873e702c2
Emanuel Santana <emanuelmunoz987@hotmail.com>
2025-06-01 22:13:32 -0600

    Creación de la app MAUI vacía y modificación del Colors.xaml para colocarle los colores adecuados para la app.

---
+++ Comerzo/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Comerzo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
